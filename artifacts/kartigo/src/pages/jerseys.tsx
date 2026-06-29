9255d646971f437bc58603c1779befe93278610f
zenithsui <zenithsuiyt@gmail.com>
2026-06-29 11:31:00 +0530

    feat: add Jerseys page

---
+++ artifacts/kartigo/src/pages/jerseys.tsx
@@ -0,0 +1,12 @@
+import ProductListing from "@/components/products/ProductListing";
+
+export default function JerseysPage() {
+  return (
+    <ProductListing
+      category="jerseys"
+      title="Jerseys"
+      icon="🎽"
+      description="Football, cricket, basketball, and sports jerseys from top clubs and teams"
+    />
+  );
+}
